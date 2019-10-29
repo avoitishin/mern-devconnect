9d291e2f3451386174a4b5876413a9fcb737413b
Andrey Voitishin <a.voitishin@gmail.com>
2019-10-29 16:32:25 -0400

    Added test API routes and local MongoDB connection

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const app = express();
 // Connect Database
 connectDB();
 
-app.get('/', (req, res) => res.send('API Running'));
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
