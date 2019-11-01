383da1f4a6898f6da064d80b478425ff89c8b7c2
Andrey Voitishin <a.voitishin@gmail.com>
2019-10-31 21:56:43 -0400

    Fixed populate() and added get all profiles route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const router = express.Router();
-const auth = require('../../middleware/auth');
-const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
 // @route   GET api/profile/me
 // @desc    Get current user's profile
@@ -13,7 +14,8 @@ router.get('/me', auth, async (req, res) => {
   try {
     const profile = await Profile.findOne({ user: req.user.id }).populate(
       'user',
-      ['name', 'avatar']
+      ['name', 'avatar'],
+      User
     );
 
     if (!profile) {
@@ -112,5 +114,22 @@ router.post(
     }
   }
 );
+
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate(
+      'user',
+      ['name', 'avatar'],
+      User
+    );
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
